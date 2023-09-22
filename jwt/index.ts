356411e790a9479f9624749b9635dbc6d198b675
Vladimir Dementyev <palkan@hey.com>
2023-09-21 20:59:30 -0700

    - fix jwt verification

--- jwt/index.ts
+++ jwt/index.ts
@@ -3,31 +3,33 @@ import * as jose from "jose";
 export type IdentifiersMap = Record<string, any>;
 
 export interface IIdentificator<I extends IdentifiersMap = {}> {
-  verify: (token: string | undefined) => boolean;
-  verifyAndFetch(token: string | undefined): Promise<I | null>;
+  verify: (token: string) => Promise<true>;
+  verifyAndFetch(token: string): Promise<I | null>;
   generateToken: (identifiers: I) => Promise<string>;
 }
 
 export const identificator = <I extends IdentifiersMap>(
   secret: string,
-  exp: string | number,
+  exp: string | number
 ) => {
   const secretEncoder = new TextEncoder().encode(secret);
   const alg = "HS256";
 
   return {
-    verify: (token: string | undefined) => {
-      if (token) return !!jose.jwtVerify(token, secretEncoder);
-    },
-    verifyAndFetch: async (token: string | undefined) => {
-      if (token) {
-        const { payload } = await jose.jwtVerify(token, secretEncoder, {
-          algorithms: [alg],
-        });
+    verify: async (token: string) => {
+      await jose.jwtVerify(token, secretEncoder, {
+        maxTokenAge: exp,
+      });
 
-        if (typeof payload.ext == "string") {
-          return JSON.parse(payload.ext) as I;
-        }
+      return true;
+    },
+    verifyAndFetch: async (token: string) => {
+      const { payload } = await jose.jwtVerify(token, secretEncoder, {
+        algorithms: [alg],
+      });
+
+      if (typeof payload.ext == "string") {
+        return JSON.parse(payload.ext) as I;
       }
 
       return null;
@@ -36,6 +38,7 @@ export const identificator = <I extends IdentifiersMap>(
       const token = await new jose.SignJWT({ ext: JSON.stringify(identifiers) })
         .setProtectedHeader({ alg })
         .setExpirationTime(exp)
+        .setIssuedAt()
         .sign(secretEncoder);
 
       return token;
