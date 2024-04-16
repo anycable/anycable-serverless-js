3ca79c77a616a8af14d893a482b9186f372f8a3a
Vladimir Dementyev <palkan@hey.com>
2024-04-16 16:28:14 -0700

    feat: support passing metadata to broadcast

--- src/broadcast/index.ts
+++ src/broadcast/index.ts
@@ -1,5 +1,17 @@
 export type IBroadcast = (stream: string, data: any) => Promise<void>
 
+export type IMetadata = {
+  transient?: boolean
+  exclude_socket?: string
+  broadcast_type?: string
+}
+
+export type IPayload = {
+  stream: string
+  data: string
+  meta?: IMetadata
+}
+
 export const broadcaster = (
   url: string,
   secret: string | undefined
@@ -12,14 +24,24 @@ export const broadcaster = (
     broadcastHeaders['Authorization'] = `Bearer ${secret}`
   }
 
-  const broadcast = async (stream: string, data: any) => {
+  const broadcast = async (
+    stream: string,
+    data: any,
+    meta: IMetadata | undefined = undefined
+  ) => {
+    const payload: IPayload = {
+      stream,
+      data: JSON.stringify(data)
+    }
+
+    if (meta) {
+      payload.meta = meta
+    }
+
     const res = await fetch(url, {
       method: 'POST',
       headers: broadcastHeaders,
-      body: JSON.stringify({
-        stream,
-        data: JSON.stringify(data)
-      })
+      body: JSON.stringify(payload)
     })
 
     if (!res.ok) {
